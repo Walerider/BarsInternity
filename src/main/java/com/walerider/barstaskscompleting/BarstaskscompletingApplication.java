5472d9ef0b273407bb5628ce73af7de28e966fa8
waler <walerider.killeron@yandex.ru>
2024-12-25 17:32:17 +0400

    start

---
+++ src/main/java/com/walerider/barstaskscompleting/BarstaskscompletingApplication.java
@@ -0,0 +1,13 @@
+package com.walerider.barstaskscompleting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarstaskscompletingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarstaskscompletingApplication.class, args);
+	}
+
+}
